4819fa90cbaeb9319fca138eba1786ea9cb9d446
Madona Syombua <syombuamadona@gmail.com>
2017-11-23 00:03:03 -0600

    Update Math_Graph.java

--- Math_Graph.java
+++ Math_Graph.java
@@ -159,7 +159,7 @@ public class Math_Graph extends JPanel {
         }
         Math_Graph mainPanel = new Math_Graph(scores);
         mainPanel.setPreferredSize(new Dimension(700, 600));
-       
+       // creating the frame
         JFrame frame = new JFrame("Sample Graph");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(mainPanel);
