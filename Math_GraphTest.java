9f194058d0fa472a5ebc530d089bae4937d25540
Madona Syombua <syombuamadona@gmail.com>
2017-11-26 16:35:37 -0600

    Add files via upload

---
+++ Math_GraphTest.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package init;
+
+import static org.junit.Assert.*;
+import MathematicalProgramming.Math_Graph;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author tmuthoka
+ *
+ */
+
+public class Math_GraphTest {
+    
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link MathematicalProgramming.Math_Graph#Math_Graph(java.util.List)}.
+	 */
+	@Test
+	public void testMath_Graph() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link MathematicalProgramming.Math_Graph#setScores(java.util.List)}.
+	 */
+	@Test
+	public void testSetScores() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link MathematicalProgramming.Math_Graph#getScores()}.
+	 */
+	@Test
+	public void testGetScores() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#disable()}.
+	 */
+	@Test
+	public void testDisable() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#isDoubleBuffered()}.
+	 */
+	@Test
+	public void testIsDoubleBuffered() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#setVisible(boolean)}.
+	 */
+	@Test
+	public void testSetVisible() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#hide()}.
+	 */
+	@Test
+	public void testHide() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#getX()}.
+	 */
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link javax.swing.JComponent#getY()}.
+	 */
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+}
